d30ca7ceaa0713307e00c3623dcfa5704f5f1624
Franco C Sanchez <francocsanchez@gmail.com>
2023-08-16 19:32:07 -0300

    Init

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.containerTxt}>Hola Coder!</Text>
+      <View style={styles.containerBox}>
+        <Text style={styles.boxTxt}>@francocsanchez</Text>
+      </View>
+      <Text>Red social para adopcion de mascotas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerTxt: {
+    fontSize: 30,
+  },
+  containerBox: {
+    marginVertical: 10,
+    backgroundColor: "#917081",
+    borderRadius: 10,
+  },
+  boxTxt: {
+    color: "#fff",
+    paddingHorizontal: 30,
+    paddingVertical: 8,
+    fontWeight: "bold",
   },
 });
