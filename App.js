8c059064c7eb9464b897b7e4acb0817bb27a7722
Franco C Sanchez <francocsanchez@gmail.com>
2023-08-16 21:06:22 -0300

    add Text

--- App.js
+++ App.js
@@ -1,14 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 
 export default function App() {
+  const handleLinkPress = (url) => {
+    Linking.openURL(url);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.containerTxt}>Hola Coder!</Text>
       <View style={styles.containerBox}>
-        <Text style={styles.boxTxt}>@francocsanchez</Text>
+        <Text style={styles.boxTxt}>Franco Sanchez</Text>
       </View>
-      <Text>Red social para adopcion de mascotas</Text>
+      <View style={styles.table}>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.cell, styles.instagramCell]}
+            onPress={() =>
+              handleLinkPress("https://www.instagram.com/francocsanchez")
+            }
+          >
+            <Text style={styles.cellText}>Instagram</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.cell, styles.twitterCell]}
+            onPress={() =>
+              handleLinkPress("https://twitter.com/francocsanchez")
+            }
+          >
+            <Text style={styles.cellText}>Twitter</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.cell, styles.linkedinCell]}
+            onPress={() =>
+              handleLinkPress("https://www.linkedin.com/in/francocsanchez/")
+            }
+          >
+            <Text style={styles.cellText}>LinkedIn</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.cell, styles.githubCell]}
+            onPress={() =>
+              handleLinkPress("https://github.com/francocsanchez/")
+            }
+          >
+            <Text style={styles.cellText}>GitHub</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,16 +74,48 @@ const styles = StyleSheet.create({
   },
   containerTxt: {
     fontSize: 30,
+    marginBottom: 10,
   },
   containerBox: {
-    marginVertical: 10,
-    backgroundColor: "#917081",
-    borderRadius: 10,
+    marginBottom: 20,
   },
   boxTxt: {
-    color: "#fff",
-    paddingHorizontal: 30,
-    paddingVertical: 8,
     fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  table: {
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  cell: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#D1D1D1",
+    borderRadius: 8,
+    marginHorizontal: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellText: {
+    textAlign: "center",
+    fontSize: 18,
+    color: "#fff",
+  },
+  instagramCell: {
+    backgroundColor: "#E4405F",
+  },
+  twitterCell: {
+    backgroundColor: "#1DA1F2",
+  },
+  linkedinCell: {
+    backgroundColor: "#0A66C2",
+  },
+  githubCell: {
+    backgroundColor: "#333",
   },
 });
