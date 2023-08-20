d6ac16b764390f46e4e78f7c2a1a1bb66f58c081
Franco C Sanchez <francocsanchez@gmail.com>
2023-08-20 19:45:43 -0300

    View login

--- App.js
+++ App.js
@@ -1,64 +1,60 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
+  TextInput,
+  Button,
+  Image,
+  Text,
   TouchableOpacity,
-  Linking,
 } from "react-native";
 
 export default function App() {
-  const handleLinkPress = (url) => {
-    Linking.openURL(url);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("Email:", email);
+    console.log("Contraseña:", password);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.containerTxt}>Hola Coder!</Text>
-      <View style={styles.containerBox}>
-        <Text style={styles.boxTxt}>Franco Sanchez</Text>
+      <View style={styles.titleContainer}>
+        <Text style={[styles.title, styles.titleAdop]}>Adop</Text>
+        <Text style={[styles.title, styles.titleTio, styles.titleTioHighlight]}>
+          Tio
+        </Text>
       </View>
-      <View style={styles.table}>
-        <View style={styles.row}>
-          <TouchableOpacity
-            style={[styles.cell, styles.instagramCell]}
-            onPress={() =>
-              handleLinkPress("https://www.instagram.com/francocsanchez")
-            }
-          >
-            <Text style={styles.cellText}>Instagram</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity
-            style={[styles.cell, styles.twitterCell]}
-            onPress={() =>
-              handleLinkPress("https://twitter.com/francocsanchez")
-            }
-          >
-            <Text style={styles.cellText}>Twitter</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity
-            style={[styles.cell, styles.linkedinCell]}
-            onPress={() =>
-              handleLinkPress("https://www.linkedin.com/in/francocsanchez/")
-            }
-          >
-            <Text style={styles.cellText}>LinkedIn</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity
-            style={[styles.cell, styles.githubCell]}
-            onPress={() =>
-              handleLinkPress("https://github.com/francocsanchez/")
-            }
-          >
-            <Text style={styles.cellText}>GitHub</Text>
-          </TouchableOpacity>
-        </View>
+      <Image
+        source={require("./assets/logo.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
+        <Button title="Iniciar Sesión" onPress={handleLogin} color="#000" />
+        <View style={styles.horizontalLine}></View>
+        <TouchableOpacity>
+          <Text style={styles.linkText}>¿Olvidaste tu contraseña?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.linkText}>Registrarte</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -68,54 +64,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#539fa2",
     alignItems: "center",
     justifyContent: "center",
   },
-  containerTxt: {
-    fontSize: 30,
-    marginBottom: 10,
-  },
-  containerBox: {
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
   },
-  boxTxt: {
-    fontWeight: "bold",
+  title: {
+    fontSize: 40,
     textTransform: "uppercase",
   },
-  table: {
-    alignItems: "center",
-    marginHorizontal: 20,
+  titleAdop: {
+    color: "#333",
+    fontWeight: "300",
   },
-  row: {
-    flexDirection: "row",
+  titleTio: {
+    color: "#666",
+    fontWeight: "600",
+  },
+  image: {
+    width: "100%",
+    height: 300,
     marginBottom: 10,
   },
-  cell: {
-    flex: 1,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "#D1D1D1",
-    borderRadius: 8,
-    marginHorizontal: 5,
-    alignItems: "center",
-    justifyContent: "center",
+  inputContainer: {
+    width: "80%",
   },
-  cellText: {
-    textAlign: "center",
-    fontSize: 18,
+  input: {
+    backgroundColor: "#72b1a4",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
     color: "#fff",
   },
-  instagramCell: {
-    backgroundColor: "#E4405F",
+  horizontalLine: {
+    borderBottomColor: "#000",
+    borderBottomWidth: 2,
+    marginVertical: 10,
   },
-  twitterCell: {
-    backgroundColor: "#1DA1F2",
-  },
-  linkedinCell: {
-    backgroundColor: "#0A66C2",
-  },
-  githubCell: {
-    backgroundColor: "#333",
+  linkText: {
+    color: "#000",
+    marginBottom: 10,
+    textAlign: "center",
   },
 });
